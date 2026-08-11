18e64056319965fad4838f0d6a8427ecd1a604d6
Jeremiah-code-source <kenanprins274@gmail.com>
2026-08-11 10:00:17 +0200

    Allow CORS for new domain; fix typo in index.html
    Added 'https://birthday-surprise-3k5.pages.dev' to allowed CORS origins in cloudflare-worker.js. Corrected "HAPPY BIRTHDAT!🎁" to "HAPPY BIRTHDAY!🎁" in index.html.

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -28,6 +28,7 @@ const ALLOWED_ORIGINS = [
     'https://birthday-surprise.kenanprins274.workers.dev',
     'https://birthday-video-upload.kenanprins274.workers.dev',
     'https://birthday-upload.kenanprins274.workers.dev',
+    'https://birthday-surprise-3k5.pages.dev',
     'https://birthday-surprise-ten-lilac.vercel.app',
     'https://jeremiah-code-source.github.io',
     'http://localhost',
